91a0dfaf0091438b5de97b8a806d05cc1d0669b4
anikinkirill <anikinkirill@yandex-team.ru>
2023-06-05 18:18:00 +0300

    Create DI layer

---
+++ androidApp/src/main/java/com/anikinkirill/playground/android/AppApplication.kt
@@ -0,0 +1,15 @@
+package com.anikinkirill.playground.android
+
+import android.app.Application
+import com.anikinkirill.playground.di.getCommonModules
+import org.koin.core.context.startKoin
+
+class AppApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            modules(getCommonModules())
+        }
+    }
+}
--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -1,10 +1,22 @@
 import SwiftUI
+import shared
 
 @main
 struct iOSApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self)
+    var appDelegate: AppDelegate
+    
 	var body: some Scene {
 		WindowGroup {
 			ContentView()
 		}
 	}
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        KoinStarterKt.doInitKoin()
+        return true
+    }
 }
