ed07b633092d566ded673153b7132fdbf126916f
anikinkirill <anikinkirill@yandex-team.ru>
2023-07-06 20:40:16 +0300

    Done 11_dispatcher_3 branch

---
+++ shared/src/iosMain/kotlin/com/anikinkirill/playground/util/Dispatcher.kt
@@ -0,0 +1,9 @@
+package com.anikinkirill.playground.util
+
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+
+actual fun provideDispatcher(): Dispatcher = object : Dispatcher {
+    override val io: CoroutineDispatcher
+        get() = Dispatchers.Default
+}
