18c889242ba4c3bfd6c2ff36747882d8539c0168
anikinkirill <anikinkirill@yandex-team.ru>
2023-07-06 20:19:52 +0300

    Done 3 branch

---
+++ shared/src/commonMain/kotlin/com/anikinkirill/playground/navigation/NavigationGraph.kt
@@ -0,0 +1,16 @@
+package com.anikinkirill.playground.navigation
+
+import androidx.compose.runtime.Composable
+import ru.alexgladkov.odyssey.compose.extensions.screen
+import ru.alexgladkov.odyssey.compose.navigation.RootComposeBuilder
+
+@Composable
+fun RootComposeBuilder.navigationGraph() {
+    screen(name = "user_list") {
+
+    }
+
+    screen(name = "user_post") {
+
+    }
+}
