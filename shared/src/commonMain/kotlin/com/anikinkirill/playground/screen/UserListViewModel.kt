9bfe3bd84579c2fc79cfa9f884f72fd39719108a
anikinkirill <anikinkirill@yandex-team.ru>
2023-07-06 20:24:39 +0300

    Done 6 branch

---
+++ shared/src/commonMain/kotlin/com/anikinkirill/playground/screen/UserListViewModel.kt
@@ -0,0 +1,26 @@
+package com.anikinkirill.playground.screen
+
+import com.adeo.kviewmodel.BaseSharedViewModel
+import kotlinx.coroutines.launch
+
+class UsersListViewModel : BaseSharedViewModel<UsersListViewState, UsersListAction, UsersListEvent>(
+    initialState = UsersListViewState.Idle
+) {
+
+    override fun obtainEvent(viewEvent: UsersListEvent) {
+        when (viewEvent) {
+            is UsersListEvent.ActionInvoked -> viewAction = null
+            is UsersListEvent.LoadUsers -> loadUsers()
+            is UsersListEvent.UserClick -> onUserClick(viewEvent.id)
+        }
+    }
+
+    private fun loadUsers() {
+        // TODO
+    }
+
+    private fun onUserClick(userId: Int) {
+        // TODO
+    }
+
+}
