661a4425f654c3740d1a2415cdd397d10ef28e92
anikinkirill <anikinkirill@yandex-team.ru>
2023-07-06 20:22:32 +0300

    Done 5 branch

---
+++ shared/src/commonMain/kotlin/com/anikinkirill/playground/screen/UserListScreenStates.kt
@@ -0,0 +1,29 @@
+package com.anikinkirill.playground.screen
+
+sealed class UsersListEvent {
+    object LoadUsers : UsersListEvent()
+
+    data class UserClick(
+        val id: Int,
+    ) : UsersListEvent()
+
+    object ActionInvoked : UsersListEvent()
+}
+
+sealed class UsersListAction {
+    data class NavigateToUserPostsScreen(
+        val id: Int,
+    ) : UsersListAction()
+}
+
+sealed class UsersListViewState {
+    object Idle : UsersListViewState()
+
+    object Loading : UsersListViewState()
+
+    data class Data(val users: List<String>) : UsersListViewState()
+
+    data class Error(
+        val errorMessage: String
+    ) : UsersListViewState()
+}
